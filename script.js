aa35609b2eb1ecb916356ec6c71e64ce7ab5af88
palaksaxenajaipur <palaksaxenajaipur2006@gmail.com>
2026-02-08 21:27:44 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function submitForm() {
-    let name = document.getElementById("name").value;
-    if (name === "") {
-        alert("Please enter your name.");
-        return false;
-    }
-    alert("Application submitted successfully!");
-    return true;
-}
